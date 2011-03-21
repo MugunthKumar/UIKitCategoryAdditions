fd4e3d1cb18ba08dc916036947144326134cb567
Mugunth <mugunth.kumar@gmail.com>
2011-03-21 09:12:26 +0800

    Initial Commit

---
+++ UIAlertViewCategory/UIAlertViewCategoryViewController.h
@@ -0,0 +1,15 @@
+//
+//  UIAlertViewCategoryViewController.h
+//  UIAlertViewCategory
+//
+//  Created by Mugunth on 21/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAlertViewCategoryViewController : UIViewController {
+    
+}
+
+@end
---
+++ UIAlertViewCategory/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UIAlertViewCategory
+//
+//  Created by Mugunth on 21/03/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
